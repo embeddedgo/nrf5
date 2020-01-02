933bbf03d1ace84a87c7fed20c52d44203dde5be
Michał Derkacz <michal@Lnet.pl>
2020-01-02 01:29:53 +0100

    pca10059/examples/blinky: enable debug output (TRACESWO) on P1.00

--- devboard/pca10059/examples/blinky/main.go
+++ devboard/pca10059/examples/blinky/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/embeddedgo/nrf5/devboard/pca10059/board/buttons"
 	"github.com/embeddedgo/nrf5/devboard/pca10059/board/leds"
+	"github.com/embeddedgo/nrf5/hal/clock"
 )
 
 func delay() {
@@ -20,21 +21,26 @@ func delay() {
 }
 
 func main() {
+	clock.StoreTRACECONFIG(clock.T4MHz, clock.Serial) // enable SWO on P1.00
 	for {
 		leds.Blue.SetOff()
 		leds.User.SetOn()
+		println("user")
 		delay()
 
 		leds.User.SetOff()
 		leds.Red.SetOn()
+		println("red")
 		delay()
 
 		leds.Red.SetOff()
 		leds.Green.SetOn()
+		println("green")
 		delay()
 
 		leds.Green.SetOff()
 		leds.Blue.SetOn()
+		println("blue")
 		delay()
 	}
 }
