5cfce13e083214856f518011c64c311e2963025c
Michał Derkacz <michal@Lnet.pl>
2020-02-15 01:06:40 +0100

    sd: remove AppRAMBase function

--- softdevice/s140/sd/rambase.s
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 Michal Derkacz. All rights reserved.
-
-#include "textflag.h"
-
-TEXT ·AppRAMBase(SB),NOSPLIT|NOFRAME,$0
-	MOVW  $runtime·ramstart(SB), R0
-	MOVW  R0, ret+0(FP)
-	RET
