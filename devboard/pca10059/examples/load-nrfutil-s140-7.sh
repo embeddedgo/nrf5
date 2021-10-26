5f0a1ea80caac23179ddf916540af7ee4620fa49
Michał Derkacz <michal@Lnet.pl>
2021-10-26 19:23:27 +0200

    pca10059/examples: fix load-nrfutil-s140-7.sh, private demo key

--- devboard/pca10059/examples/load-nrfutil-s140-7.sh
+++ devboard/pca10059/examples/load-nrfutil-s140-7.sh
@@ -5,9 +5,9 @@ port=/dev/ttyACM0
 
 hw=52
 
-sdreq=0xCA
+sdreq=0xCA,0x0100
 
 s140_nrf52_7_0_1_softdevice_hex=0xCA
-s140_nrf52_7_2_0_softdevice_hex=0x100
+s140_nrf52_7_2_0_softdevice_hex=0x0100
 
 . ../../../../../scripts/load-nrfutil.sh $@
