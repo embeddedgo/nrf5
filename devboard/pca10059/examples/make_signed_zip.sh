527a6586e912d6eb1f3779558e4a06f13ea345c6
Michał Derkacz <michal@Lnet.pl>
2021-10-29 19:03:01 +0200

    move BLE bootloader to bootloader dir

--- devboard/pca10059/examples/make_signed_zip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-appversion=1
-key=../ble_bootloader.key
-
-name=$(basename $(pwd))
-
-nrfutil pkg generate --hw-version 52 --sd-req 0xCA,0x0100 --key-file $key --application $name.hex --application-version $appversion $name.zip
---
+++ devboard/pca10059/examples/signed-zip.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+appversion=1
+key=../bootloader/private.key
+
+name=$(basename $(pwd))
+
+nrfutil pkg generate --hw-version 52 --sd-req 0xCA,0x0100 --key-file $key --application $name.hex --application-version $appversion $name.zip
