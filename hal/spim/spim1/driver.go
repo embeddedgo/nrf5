8528398320bd16dd53bbcdd2dd85daac8d93fa10
Michał Derkacz <michal@Lnet.pl>
2021-11-13 00:45:13 +0100

    spim: fix subpackage names

--- hal/spim/spim1/driver.go
+++ hal/spim/spim1/driver.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package spim0
+package spim1
 
 import (
 	"embedded/rtos"
--- hal/spim/spim2/driver.go
+++ hal/spim/spim2/driver.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package spim0
+package spim2
 
 import (
 	"embedded/rtos"
